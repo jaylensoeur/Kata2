d3a9367669a987dcf98d5d2cd704068981c2be29
Joyce.Yeung <Joyce.Yeung@myob.com>
2020-08-20 16:41:25 +1000

    Completed fizzbuzz logic

---
+++ Kata2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Kata2
+{
+    class Program
+    {
+        private const int StartNumber = 1; 
+        private const int EndNumber = 100;
+        private const int FizzNumber = 3;
+        private const int BuzzNumber = 5;
+
+        static void Main(string[] args)
+        {
+            string[] outputs = new string[EndNumber];
+            int indexLocation = 0;
+            for (int i = StartNumber; i <= EndNumber; i++)
+            {
+                string output = null;
+                if (i % FizzNumber == 0) output = "Fizz";
+                if (i % BuzzNumber == 0) output += "Buzz";
+                if (output == null) output = i.ToString();
+                outputs[indexLocation] = output;
+                indexLocation++;
+            }
+            string outputsInString = String.Join(", ", outputs);
+            Console.WriteLine(outputsInString);
+        }
+    }
+}
